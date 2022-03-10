c83447eaa546129939734518647711e54c4980c7
edemuner <eduardood15@hotmail.com>
2022-03-10 14:13:50 -0300

    added a .env file with key and value, made the json response conditional to the value of this pair

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const dotenv = require('dotenv').config()
 
 app.use('/public', express.static(__dirname + '/public'))
 
@@ -8,7 +9,11 @@ app.get('/', (req, res) => {
 })
 
 app.get('/json', (req, res) => {
-    res.json({message:'Hello json'})
+    if (process.env.MESSAGE_STYLE === 'uppercase'){
+        res.json({'message':'HELLO JSON'})
+    } else {
+        res.json({'message':'Hello json'})
+    }
 })
 
 
